78ce67b868d5b788046dc6b0bfd347d166c924fd
jeff muriuki <jeff.muriuki@studet.moringaschool.com>
2024-03-26 21:36:48 +0300

    loops

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        messages.push(message)
+    }
+    return messages;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(number) {
+    let i = number;   
+   
+    while (i>=0) {
+        console.log(i);
+        i--;
+    }
+}
+countDown();
